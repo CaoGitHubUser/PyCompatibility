abaf3768f51d2da3d23402702aaa5881b92fe9a6
Cao <139331248+CaoGitHubUser@users.noreply.github.com>
2023-11-03 16:46:48 +0800

    Fix compatibility issue

--- run_tests.py
+++ run_tests.py
@@ -14,7 +14,8 @@ LOG: logging.Logger = logging.getLogger()
 LOG.setLevel(logging.DEBUG)
 log.ensure_color_log()
 
-TEST_FILE: re.Pattern[str] = re.compile(r"(test_(.*).py)|((.*)_test.py)")
+# TODO: use annotation instead when Python 3.8 EOL
+TEST_FILE = re.compile(r"(test_(.*).py)|((.*)_test.py)")  # type: re.Pattern[str]
 
 
 def main() -> int:
@@ -28,6 +29,7 @@ def main() -> int:
             if TEST_FILE.fullmatch(file) is not None:
                 test_files.append(f"{path}/{file}")
 
+    # TODO: use f"{'\n'.join(...)}" instead when Python 3.11 EOL
     LOG.info("Found these test files:\n" "{}".format("\n".join(test_files)))
 
     LOG.info("Running checks...")
